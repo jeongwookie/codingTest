129b61db404d2257ecc58cef6a31e3ccaa28e7fb
Jeongwook Kim <jeongwookie93@gmail.com>
2022-01-12 15:41:04 +0900

    baekjoon (백트래킹) : 스도쿠 문제 풀이

--- baekjoon/백트래킹/스도쿠.py
+++ baekjoon/백트래킹/스도쿠.py
@@ -5,11 +5,9 @@ def row_check(row_idx, x):
 
 
 def col_check(col_idx, x):
-    _list = []
     for i in range(N):
-        _list.append(graph[i][col_idx])
-    if x in _list:
-        return False
+        if x == graph[i][col_idx]:
+            return False
     return True
 
 
@@ -24,17 +22,17 @@ def square_check(row_idx, col_idx, x):
 
 
 def dfs(i):
-    if i == len(empty):
+    if i == len(blank):
         # 최종 형태의 스도쿠 출력
         for row in graph:
             for v in row:
                 print(v, end=' ')
             print()
-        return
+        sys.exit()  # 하나 찾으면 프로그램 자체를 종료시킴
 
     # 빈칸에 숫자 채워넣기
     for x in range(1, 10):
-        row_idx, col_idx = empty[i][0], empty[i][1]
+        row_idx, col_idx = blank[i]
         if row_check(row_idx, x) and col_check(col_idx, x) and square_check(row_idx, col_idx, x):
             graph[row_idx][col_idx] = x # 1~9 사이의 숫자를 성공적으로 할당
             dfs(i+1) # 다음 빈칸으로 이동
@@ -44,13 +42,12 @@ def dfs(i):
 if __name__ == "__main__":
     import sys; readline = sys.stdin.readline
     graph = []
-    for _ in range(9):
+    N = 9  # 스도쿠 판 크기
+    for _ in range(N):
         graph.append(list(map(int, readline().split())))
 
     # 스도쿠 규칙에 맞게 풀이.
     # 0 인 지점에 숫자를 채워넣어야 함.
     # 0 인 지점을 처음에 다 센다음 해당 갯수에 도달하면 종료
-
-    N = 9  # 스도쿠 판 크기
-    empty = [(i, j) for i in range(N) for j in range(N) if graph[i][j] == 0]
+    blank = [(i, j) for i in range(N) for j in range(N) if graph[i][j] == 0]
     dfs(0)
